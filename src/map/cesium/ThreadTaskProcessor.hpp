4d1df5e4d49204f56a83bc5761e3c2dd20f77277
Long 2k6 <skullmuder@gmail.com>
2025-09-11 22:25:38 +0700

    update thread pool

--- src/map/cesium/ThreadTaskProcessor.hpp
+++ src/map/cesium/ThreadTaskProcessor.hpp
@@ -5,15 +5,15 @@
 #include <thread>
 #include <threadpool/BS_thread_pool.hpp>
 
-
+static BS::thread_pool<BS::tp::none> pool;
 namespace CesiumNativeTests {
 class ThreadTaskProcessor : public CesiumAsync::ITaskProcessor {
 public:
     virtual void startTask(std::function<void()> f) override {
-        std::thread(f).detach();
-        //pool.detach_task(f);
+        //std::thread(f).detach();
+        pool.detach_task(f);
     }; 
 protected: 
-    //BS::thread_pool<BS::tp::none> pool;
+    //
 };
 } // namespace CesiumNativeTests
